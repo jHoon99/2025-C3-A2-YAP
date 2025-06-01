952f1a7f01601a89ec6e667f44a6ded7cfb0b183
Hong <dbsghdz1@gmail.com>
2025-06-01 14:35:36 +0900

    Feat: spacing추가

---
+++ YAP/YAP/Resource/DesignSystem/Spacing/Spacing.swift
@@ -0,0 +1,15 @@
+//
+//  Spacing.swift
+//  YAP
+//
+//  Created by Hong on 6/1/25.
+//
+
+import SwiftUI
+
+public enum Spacing {
+  public static let small: CGFloat = 8
+  public static let medium: CGFloat = 16
+  public static let large: CGFloat = 24
+  public static let extrLarge: CGFloat = 32
+}
