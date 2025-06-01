ca408598030a4b9a1cac0a01b6785ed6cb418914
Hong <dbsghdz1@gmail.com>
2025-06-01 15:06:38 +0900

    Feat: Cta버튼 추가

---
+++ YAP/YAP/Resource/DesignSystem/ButtonStyle/CtaButtonLabel.swift
@@ -0,0 +1,22 @@
+//
+//  CtaButtonLabel.swift
+//  YAP
+//
+//  Created by Hong on 6/1/25.
+//
+
+/// CTA 버튼에 사용되는 버튼 이름 케이스
+///
+/// - Cases:
+///   - next: "다음"
+///   - calculate: "계산하기"
+///   - start: "시작하기"
+///   - add: "추가하기"
+///   - eat: "이대로 먹기"
+public enum CtaButtonLabel: String {
+  case next = "다음"
+  case calculate = "계산하기"
+  case start = "시작하기"
+  case add = "추가하기"
+  case eat = "이대로 먹기"
+}
