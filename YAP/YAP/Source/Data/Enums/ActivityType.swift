0b18262f77f7e2334fb69b8bc2e9f3a9dc84c458
YeoSeongil <seongil_yeo@naver.com>
2025-06-03 21:45:44 +0900

    [#23] feat: 활동 수준 선택 UI 구현

--- YAP/YAP/Source/Data/Enums/ActivityType.swift
+++ YAP/YAP/Source/Data/Enums/ActivityType.swift
@@ -7,10 +7,29 @@
 
 import Foundation
 
-enum ActivityType: String, CaseIterable, Codable {
+public enum ActivityType: String, CaseIterable, Codable {
   case verylittleActivity = "매우 적은 활동"
   case littleActivity = "적은 활동"
   case middleActivity = "중간 활동"
   case vigorousActivity = "격한 활동"
   case veryVigorousActivity = "매우 격한 활동"
+  
+  var caption: String {
+    switch self {
+    case .verylittleActivity:
+      return "거의 하루 종일 앉아서 지내는 좌식 생활"
+      
+    case .littleActivity:
+      return "가끔 가볍게 움직이거나 운동하는 라이트한 생활"
+      
+    case .middleActivity:
+      return "매일은 아니어도 종종 운동하고 적당히 움직이는 균형 잡힌 생활"
+      
+    case .vigorousActivity:
+      return "매일 꾸준한 운동과 활발한 움직임이 있는 고활동 생활"
+      
+    case .veryVigorousActivity:
+      return "지속적인 신체 활동과 고강도 운동이 포함된 매우 높은 활동 생활"
+    }
+  }
 }
