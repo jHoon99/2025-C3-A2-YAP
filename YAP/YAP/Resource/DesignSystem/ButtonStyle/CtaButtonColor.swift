80e8bd3194b2e63ab32da8197e68708c6a95b8ad
YeoSeongil <seongil_yeo@naver.com>
2025-06-03 21:45:09 +0900

    [#23] refactor: CtaButton 기능 수정
    ButtonLabel Case 추가, ButtonColor Case 추가, titleColor/bgColor 커스텀 해서 사용할 수 있도록 수정

---
+++ YAP/YAP/Resource/DesignSystem/ButtonStyle/CtaButtonColor.swift
@@ -0,0 +1,26 @@
+//
+//  CtaButtonColor.swift
+//  YAP
+//
+//  Created by 여성일 on 6/3/25.
+//
+
+/// CTA 버튼에 사용되는 컬러 케이스
+///
+/// - Cases:
+///   - main: 메인 색상
+///   - primaryActive: 메인 액티브
+///   - primaryHover: 메인 호버
+///   - light: 라이트 색상
+///   - lightActive: 라이트 액티브
+///   - lightHover: 라이트 호버
+///   - white: 흰색
+public enum CtaButtonColor: String {
+  case main
+  case primaryActive
+  case primaryHover
+  case light
+  case lightActive
+  case lightHover
+  case white
+}
