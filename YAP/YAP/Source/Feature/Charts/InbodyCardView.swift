ff29a3f55a77a57007304414d2d39d18233576e4
cherry-go-round <hongseokhodev@gmail.com>
2025-06-04 00:00:32 +0900

    Refactor: 반복되는 InbodyItemView 분리

--- YAP/YAP/Source/Feature/Charts/InbodyCardView.swift
+++ YAP/YAP/Source/Feature/Charts/InbodyCardView.swift
@@ -15,64 +15,39 @@ struct InbodyCardView: View {
       Text(inbody.date.formattedYMD)
         .font(.pretendard(type: .medium, size: 14))
         .foregroundStyle(.darkHover)
+      
       HStack {
-        VStack(alignment: .leading, spacing: 12) {
-          Text("몸무게")
-            .font(.pretendard(type: .regular, size: 14))
-            .foregroundStyle(.subText)
-          HStack(spacing: 4) {
-            Text("\(Int(inbody.weight))")
-              .font(.pretendard(type: .regular, size: 20))
-            Text("kg")
-              .font(.pretendard(type: .regular, size: 14))
-          }
-        }
-        
+        InbodyItemView(name: "몸무게", value: "\(Int(inbody.weight))", unit: "kg")
         Spacer()
-        
-        VStack(alignment: .leading, spacing: 12) {
-          Text("체지방량")
-            .font(.pretendard(type: .regular, size: 14))
-            .foregroundStyle(.subText)
-          HStack(spacing: 4) {
-            Text("\(Int(inbody.skeletalMuscleMass))")
-              .font(.pretendard(type: .regular, size: 20))
-            Text("kg")
-              .font(.pretendard(type: .regular, size: 14))
-          }
-        }
-        
+        InbodyItemView(name: "체지방량", value: "\(Int(inbody.bodyFatMass))", unit: "kg")
         Spacer()
-        
-        VStack(alignment: .leading, spacing: 12) {
-          Text("골격근량")
-            .font(.pretendard(type: .regular, size: 14))
-            .foregroundStyle(.subText)
-          HStack(spacing: 4) {
-            Text("\(Int(inbody.bodyFatMass))")
-              .font(.pretendard(type: .regular, size: 20))
-            Text("kg")
-              .font(.pretendard(type: .regular, size: 14))
-          }
-        }
-        
+        InbodyItemView(name: "골격근량", value: "\(Int(inbody.skeletalMuscleMass))", unit: "kg")
         Spacer()
-        
-        VStack(alignment: .leading, spacing: 12) {
-          Text("기초대사량")
-            .font(.pretendard(type: .regular, size: 14))
-            .foregroundStyle(.subText)
-          HStack(spacing: 4) {
-            Text("\(inbody.basalMetabolicRate)")
-              .font(.pretendard(type: .regular, size: 20))
-            Text("kcal")
-              .font(.pretendard(type: .regular, size: 14))
-          }
-        }
+        InbodyItemView(name: "기초대사량", value: "\(inbody.basalMetabolicRate)", unit: "kcal")
       }
     }
     .padding(.vertical, 16)
     .padding(.horizontal, 20)
+  }
+}
+
+struct InbodyItemView: View {
+  let name: String
+  let value: String
+  let unit: String
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 12) {
+      Text(name)
+        .font(.pretendard(type: .regular, size: 14))
+        .foregroundStyle(.subText)
+      HStack(spacing: 4) {
+        Text(value)
+          .font(.pretendard(type: .regular, size: 20))
+        Text(unit)
+          .font(.pretendard(type: .regular, size: 14))
+      }
+    }
   }
 }
 
