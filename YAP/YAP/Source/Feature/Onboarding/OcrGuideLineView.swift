4ee3fe6ece5236d45f5cb750e90ef5d8edaea218
YeoSeongil <seongil_yeo@naver.com>
2025-06-03 21:40:47 +0900

    [#23] feat: 온보딩 ocrView 구현

---
+++ YAP/YAP/Source/Feature/Onboarding/OcrGuideLineView.swift
@@ -0,0 +1,55 @@
+//
+//  OcrGuideLineView.swift
+//  YAP
+//
+//  Created by 여성일 on 6/2/25.
+//
+
+import SwiftUI
+
+struct OcrGuideLineView: View {
+  private let cornerLength: CGFloat = 25
+  private let lineWidth: CGFloat = 5
+  private let color: Color = .main
+  
+  var body: some View {
+    GeometryReader { geometry in
+      ZStack {
+        Color.clear.ignoresSafeArea()
+        
+        Group {
+          cornerView
+            .position(x: 0, y: 0)
+            .offset(x: cornerLength / 2, y: cornerLength / 2)
+          
+          cornerView
+            .rotationEffect(.degrees(90))
+            .position(x: geometry.size.width, y: 0)
+            .offset(x: -cornerLength / 2, y: cornerLength / 2)
+          
+          cornerView
+            .rotationEffect(.degrees(180))
+            .position(x: geometry.size.width, y: geometry.size.height)
+            .offset(x: -cornerLength / 2, y: -cornerLength / 2)
+          
+          cornerView
+            .rotationEffect(.degrees(270))
+            .position(x: 0, y: geometry.size.height)
+            .offset(x: cornerLength / 2, y: -cornerLength / 2)
+        }
+      }
+    }
+  }
+  
+  private var cornerView: some View {
+    ZStack(alignment: .topLeading) {
+      Rectangle()
+        .fill(color)
+        .frame(width: cornerLength, height: lineWidth)
+      Rectangle()
+        .fill(color)
+        .frame(width: lineWidth, height: cornerLength)
+    }
+    .frame(width: cornerLength, height: cornerLength, alignment: .topLeading)
+  }
+}
---
+++ YAP/YAP/Source/Feature/Onboarding/OnboardingInbodyOcrView.swift
@@ -0,0 +1,75 @@
+//
+//  OnboardingInbodyOcrView.swift
+//  YAP
+//
+//  Created by 여성일 on 6/2/25.
+//
+
+import SwiftUI
+
+struct OnboardingInbodyOcrView: View {
+  @State private var isNext = false
+  @State private var value = 0.5
+  
+  var body: some View {
+    ZStack {
+      Color.clear.ignoresSafeArea()
+      
+      VStack(spacing: Spacing.large) {
+        ocrPreviewView
+        
+        ocrBottomButtonView
+      }
+    }
+    .padding(.horizontal, Spacing.medium)
+    .padding(.vertical, Spacing.extrLarge)
+    .toolbar(.hidden)
+    .navigationDestination(isPresented: $isNext) {
+      OnboardingMainView()
+    }
+  }
+  
+  private var ocrPreviewView: some View {
+    VStack(spacing: Spacing.large) {
+      Text("가이드 영역에 인바디 결과지를 맞춰주세요.")
+        .font(.pretendard(type: .bold, size: 16))
+        .foregroundStyle(.text)
+      OcrGuideLineView()
+    }
+  }
+  
+  private var ocrBottomButtonView: some View {
+    VStack(spacing: Spacing.small) {
+      Text("인바디 결과지가 인식되지 않는다면 직접 입력해주세요.")
+        .font(.pretendard(type: .light, size: 12))
+        .foregroundStyle(.text)
+      
+      Button {
+        isEditButtonTapped()
+      } label: {
+        Text("직접 입력")
+          .font(.pretendard(type: .bold, size: 16))
+          .foregroundStyle(.text)
+          .frame(maxWidth: .infinity)
+      }
+      .overlay(
+        Rectangle()
+          .frame(height: 1)
+          .frame(maxWidth: 60)
+          .foregroundStyle(.text)
+          .padding(.bottom, 0),
+        alignment: .bottom
+      )
+    }
+    .buttonStyle(.plain)
+  }
+}
+
+private extension OnboardingInbodyOcrView {
+  func isEditButtonTapped() {
+    isNext = true
+  }
+}
+#Preview {
+  OnboardingInbodyOcrView()
+}
