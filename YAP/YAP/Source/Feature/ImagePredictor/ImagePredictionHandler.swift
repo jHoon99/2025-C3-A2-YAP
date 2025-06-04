e559d37e95be3648a326ea2f3479b7353b5a74b0
Hong <dbsghdz1@gmail.com>
2025-06-04 21:13:29 +0900

    Feat: view에서 사진찍은 이미지 모델과 연동하기 위한 handler구현

---
+++ YAP/YAP/Source/Feature/ImagePredictor/ImagePredictionHandler.swift
@@ -0,0 +1,34 @@
+//
+//  CameraViewModel.swift
+//  YAP
+//
+//  Created by Hong on 6/1/25.
+//
+
+import SwiftUI
+
+@Observable
+final class ImagePredictionHandler {
+ var predictions: [ImagePredictor.Prediction] = []
+ var errorMessage: String?
+  
+  private let predictor = ImagePredictor()
+  
+  func classify(image: UIImage) {
+    errorMessage = nil
+    predictions = []
+    
+    do {
+      try predictor.makePredictions(for: image) { [weak self] result in
+        guard let self else { return }
+        DispatchQueue.main.async {
+          self.predictions = result ?? []
+        }
+      }
+    } catch {
+      DispatchQueue.main.async {
+        self.errorMessage = error.localizedDescription
+      }
+    }
+  }
+}
