4d4a35a8e18653537960b16a03f416daf5b5ba08
cherry-go-round <hongseokhodev@gmail.com>
2025-06-04 14:58:55 +0900

    Refactor: TimeRange 파일 분리

--- YAP/YAP/Source/Feature/Charts/InbodyInfoView.swift
+++ YAP/YAP/Source/Feature/Charts/InbodyInfoView.swift
@@ -54,55 +54,6 @@ struct InbodyInfoView: View {
   }
 }
 
-enum TimeRange: String, CaseIterable, Identifiable {
-  case threeMonths = "3개월"
-  case sixMonths = "6개월"
-  case oneYear = "1년"
-  case custom = "기간 설정"
-  
-  var id: String { rawValue }
-  
-  func isWithinRange(from startDate: Date, to endDate: Date) -> ((Date) -> Bool) {
-    switch self {
-    case .threeMonths:
-      guard let threeMonthsAgo = Calendar.current.date(
-        byAdding: .month,
-        value: -3,
-        to: endDate
-      ) else {
-        return { _ in false }
-      }
-      
-      return { day in day >= threeMonthsAgo}
-      
-    case .sixMonths:
-      guard let sixMonthsAgo = Calendar.current.date(
-        byAdding: .month,
-        value: -6,
-        to: endDate
-      ) else {
-        return { _ in false }
-      }
-      
-      return { day in day >= sixMonthsAgo }
-      
-    case .oneYear:
-      guard let oneYearAgo = Calendar.current.date(
-        byAdding: .year,
-        value: -1,
-        to: endDate
-      ) else {
-        return { _ in false }
-      }
-      
-      return { day in day >= oneYearAgo }
-      
-    case .custom:
-      return { day in day >= startDate && day <= endDate }
-    }
-  }
-}
-
 private struct TimeRangeSelectionButtons: View {
   @Binding var selectedRange: TimeRange
   @Binding var showRangeSelectionSheet: Bool
