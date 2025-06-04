2d13b697ad193d4bc209bac83a882c0629ff86e0
jHoon99 <eldkf1597@naver.com>
2025-06-04 21:30:09 +0900

    장바구니뷰

---
+++ YAP/YAP/Source/Feature/FoodSearch/Cart/CartItemRow.swift
@@ -0,0 +1,40 @@
+//
+//  CartItemRow.swift
+//  YAP
+//
+//  Created by 조재훈 on 6/4/25.
+//
+
+import SwiftUI
+
+struct CartItemRow: View {
+  let item: CartItem
+  
+  var body: some View {
+    HStack {
+        Text(item.foodName)
+          .font(.pretendard(type: .semibold, size: 16))
+      
+        Spacer()
+      
+        Text("\(item.calorie) kcal")
+        .font(.pretendard(type: .semibold, size: 16))
+        .foregroundColor(Color.placeholder)
+    }
+    .padding(Spacing.large)
+  }
+}
+
+#Preview {
+  CartItemRow(item:
+                CartItem(
+                  id: UUID(),
+                  foodName: "닭",
+                  calorie: 200,
+                  carbohydrate: 200,
+                  protein: 200,
+                  fat: 200,
+                  quantity: 200,
+                  unit: .gram)
+  )
+}
