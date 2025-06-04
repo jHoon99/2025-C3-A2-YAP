ddec11c9a7f37340549f48bab281ff8b3f861236
YeoSeongil <seongil_yeo@naver.com>
2025-06-04 14:07:39 +0900

    [#23] chore: 불필요한 퀵헬프 삭제

--- YAP/YAP/Resource/DesignSystem/ButtonStyle/CtaButtonColor.swift
+++ YAP/YAP/Resource/DesignSystem/ButtonStyle/CtaButtonColor.swift
@@ -5,17 +5,7 @@
 //  Created by 여성일 on 6/3/25.
 //
 
-/// CTA 버튼에 사용되는 컬러 케이스
-///
-/// - Cases:
-///   - main: 메인 색상
-///   - primaryActive: 메인 액티브
-///   - primaryHover: 메인 호버
-///   - light: 라이트 색상
-///   - lightActive: 라이트 액티브
-///   - lightHover: 라이트 호버
-///   - white: 흰색
-public enum CtaButtonColor: String {
+enum CtaButtonColor: String {
   case main
   case primaryActive
   case primaryHover
