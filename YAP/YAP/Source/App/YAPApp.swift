28cd7b94ba01e2de5d06c6099b321c40c028ff77
YeoSeongil <seongil_yeo@naver.com>
2025-06-05 16:37:16 +0900

    [#26] feat: 모델 컨테이너 주입

--- YAP/YAP/Source/App/YAPApp.swift
+++ YAP/YAP/Source/App/YAPApp.swift
@@ -5,13 +5,30 @@
 //  Created by 여성일 on 5/28/25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct YAPApp: App {
+  var yapModelContainer: ModelContainer = {
+    let schema = Schema([
+      Inbody.self,
+      CalorieRequirements.self
+    ])
+    
+    let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    
+    do {
+      return try ModelContainer(for: schema, configurations: [configuration])
+    } catch {
+      fatalError("ModelContainer err")
+    }
+  }()
+  
   var body: some Scene {
     WindowGroup {
-      OnboardingView()
+      ContentView()
     }
+    .modelContainer(yapModelContainer)
   }
 }
