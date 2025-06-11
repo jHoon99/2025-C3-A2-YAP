86adb8baaff17ab96c1190f5fdd2f71b071daccb
YeoSeongil <seongil_yeo@naver.com>
2025-06-12 00:03:21 +0900

    [#60] Spacing 오타 수정

--- YAP/YAP/Resource/DesignSystem/Spacing/Spacing.swift
+++ YAP/YAP/Resource/DesignSystem/Spacing/Spacing.swift
@@ -11,5 +11,5 @@ public enum Spacing {
   public static let small: CGFloat = 8
   public static let medium: CGFloat = 16
   public static let large: CGFloat = 24
-  public static let extrLarge: CGFloat = 32
+  public static let extraLarge: CGFloat = 32
 }
